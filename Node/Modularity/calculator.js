85ad894cafcad8819f0c8e8b161ff8e8e82a9f7e
anamikarawat <anamikarawart@gmail.com>
2022-01-08 23:33:39 +0530

    add

---
+++ Node/Modularity/calculator.js
@@ -0,0 +1,24 @@
+function add(a,b){
+    console.log("The Sum is ", a+b)
+}
+
+function sub(a,b){
+    console.log("The difference is ", a-b)
+}
+function mul(a,b){
+    console.log("The product is ", a*b)
+}
+function div(a,b){
+    console.log("The quotient is ", a+b)
+}
+
+module.exports={
+    addition : add,
+    substract : sub,
+    Multiply : mul,
+    division : div,
+}
+// module.exports is a Object provided by Node.js by
+// which you can export your functions in key value pair
+// you will use your functions with the keys you have assigned
+// to them
---
+++ Node/Modularity/moduleTest.js
@@ -0,0 +1,17 @@
+const calculatorModule = require('./calculator')/// here you have imported your module
+
+//Use your Imported Methods like this 
+calculatorModule.Multiply(2,3)
+
+calculatorModule.addition(10,20)
+
+calculatorModule.substract(30,10)
+
+calculatorModule.division(50,5)
+
+// command -node moduleTest.js
+//output
+// The product is  6
+// The Sum is  30
+// The difference is  20
+// The quotient is  55
