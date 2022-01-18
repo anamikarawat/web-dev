f777d4ce452d9aad38840d9cf809826c6a25d125
anamikarawat <anamikarawart@gmail.com>
2022-01-18 22:11:53 +0530

    add

--- File System Organizer/fo.js
+++ File System Organizer/fo.js
@@ -6,12 +6,14 @@
 // so at the end you will have a arranged set of files in specific folders
 
 //js mein input Array ke from mein jaata hai and that is array is process.argv Array
+const fs = require("fs");
+const path = require("path/posix");
 
 let inputArr = process.argv.slice(2);
 
 //[Node FO.js tree folderpath]
 
-let command = inputArr[0]
+let command = inputArr[0];
 
 
 //we use command line in this project
@@ -30,7 +32,8 @@ switch (command) {
         console.log('Tree Implemented');
         break;
     case 'organize':
-        console.log('Organize Implemented');
+        //console.log('Organize Implemented');
+        organizeFn(inputArr[1]);
         break;
     case 'help':
         //console.log('Help Implemented');
@@ -42,11 +45,36 @@ switch (command) {
 }
 
 
-function helpfn(){
+function helpfn() {
     console.log(`List of all the Commands-
                      1) Tree Command - node FO.js tree <dirname>
                      2) Organize Command- node FO.js organize <dirname>
                      3) Help Command - node FO.js help`)
- }
- 
- 
+}
+
+function organizeFn(dirpath) {
+    let destPath
+
+    if (dirpath == undefined) {
+        console.log("Please Enter a valid Directory Path");  //check whether dirpath is passed or not
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirpath); //this will tell whether the dirpath exists or not
+        //console.log(doesExist);   //true ya false return kar dega
+
+        if (doesExist == true) {
+            destPath = path.join(dirpath, 'organized_files')
+           // C:\Users\hp\Desktop\WEB DEV\test folder\organized_files       -->dirpath--> I want to create a folder in this path
+            if (fs.existsSync(destPath) == false) {
+
+                fs.mkdirSync(destPath)// we will only create a folder if it does not already exists
+            } else {
+                console.log('This folder already exists')
+            }
+        }
+        else {
+            console.log('Please enter a valid path')
+        }
+
+    }
+}
