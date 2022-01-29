085726c66f8b10721e72080116bf85d017459e79
anamikarawat <anamikarawart@gmail.com>
2022-01-30 00:10:00 +0530

    add

--- WEB SCRAPING/request.js
+++ WEB SCRAPING/request.js
@@ -3,7 +3,7 @@ const request = require('request');
 //asyncronous way
 console.log('Before')
 
-request('https://www.worldometers.info/coronavirus/', function (error, response, body) {
+request('https://www.worldometers.info/coronavirus/', function (error, response, body) {//this function is a callback function
   console.error('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
