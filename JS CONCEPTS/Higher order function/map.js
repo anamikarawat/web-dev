020dad1923a941add0fbb5f697a9a55b71f1ace8
anamikarawat <anamikarawart@gmail.com>
2022-02-01 22:18:11 +0530

    add

--- JS CONCEPTS/Higher order function/map.js
+++ JS CONCEPTS/Higher order function/map.js
@@ -1,14 +1,33 @@
+// Higher order functions are functions that operate on other functions,
+// either by taking them as arguments or by returning them.
+// In simple words,
+// A Higher-Order function is a function that receives a function as an argument
+// or returns the function as output.
+
 let arr = [2, 5, 9, 8, 6, 4, 3];
-
-//Map function
-
 // function squarer(v){
 //     return v*v;
 // }
 // let square = squarer(2)
 // console.log(square)
+
+//Map function- map is itself a function,it is a higher order function
+// map takes a callback function as an argument,
+// map will call the callback functions as many times as the elements in the array
+// map will process every value and will apply the instruction that inside the callback function
+//map returns a new array.
 let sqarr = arr.map(function squarer(v) {
     return v * v;
 
 })
 console.log(sqarr)
+console.log(arr)
+//====================================================================================
+
+let nameArr =  ["anamika" , "rawat", "annu"];
+let  nameArr2 =nameArr.map(function (v) {
+    return v ;
+
+})
+console.log(nameArr2);
+
---
+++ JS CONCEPTS/Higher order function/splitJoin.js
@@ -0,0 +1,17 @@
+//split function-
+//let str = 'I am a coder'
+//let partOfString = str.split('a')
+
+let str1 = 'anamika'
+let partOfString = str1.split('a')
+//split function will split a string according to a passed paramerter
+// and will return the left and the right part in an array
+console.log(partOfString)  //split Array
+
+
+//join
+let joinedStr = partOfString.join('a')
+// Join function will take an array and it will fill the parameter after each element 
+//of the array with the passed parameter
+// and will return the complete str
+console.log(joinedStr)  //joined string
