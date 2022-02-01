694fc49d2a1efcfe7479ba21ee394ddaf852a3e5
anamikarawat <anamikarawart@gmail.com>
2022-02-01 23:50:33 +0530

    add

--- JS CONCEPTS/Higher order function/some.js
+++ JS CONCEPTS/Higher order function/some.js
@@ -1,7 +1,10 @@
+//Some-> Condition check : To get true or false based on a condition
+//it will return true.........
 const transactions = [-1000, 3000, 4000, 2000, -898, 3800, -4500];
 
 let anyDeposit = transactions.some(function (n) {
     return n > 0
 })
+//return true - agar ek bhi element n>0 vali condition ko satisfy karega toh
 
 console.log(anyDeposit)
