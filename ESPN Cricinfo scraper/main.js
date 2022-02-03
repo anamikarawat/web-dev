a57a47907c62c72ebfa66cd966097389344c04cd
anamikarawat <anamikarawart@gmail.com>
2022-02-03 22:58:08 +0530

    add

--- ESPN Cricinfo scraper/main.js
+++ ESPN Cricinfo scraper/main.js
@@ -9,14 +9,14 @@ request(url, cb)
 function cb(error, response, html) {
     if (error) {
         console.error(error)
-    } 
+    }
     else {
         extractLink(html);
-       // console.log(html)
+        // console.log(html)
     }
 }
 //view all results- ek anchor tag h which have href hoga usmea site ka link
-function extractLink(html){
+function extractLink(html) {
     let $ = cheerio.load(html)
     //half link
     let anchorElem = $('a[data-hover="View All Results"]')
@@ -27,4 +27,33 @@ function extractLink(html){
     //full link- concatination
     let fullLink = "https://www.espncricinfo.com/" + link;
     console.log(fullLink);
+
+
+
+    getAllMatchLink(fullLink)
+}
+//link for score card of all matches
+function getAllMatchLink(uri) {
+    request(uri, function (error, response, html) {
+        if (error) {
+            console.error(error)
+        }
+        else {
+            extractAllLink(html);
+        }
+    })
+}
+
+function extractAllLink(html) {
+    let $ = cheerio.load(html)
+
+    let scoreCardArr = $('a[data-hover="Scorecard"]')
+
+    for (let i = 0; i < scoreCardArr.length; i++) {
+        let link = $(scoreCardArr[i]).attr('href');
+
+        let fullLink = 'https://www.espncricinfo.com/' + link;
+        console.log(fullLink);
+    }
+
 }
