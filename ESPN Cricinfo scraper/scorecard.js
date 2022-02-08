e772b3b721da7b0365997c752edea9df54c3a458
anamikarawat <anamikarawart@gmail.com>
2022-02-08 22:39:10 +0530

    add

--- ESPN Cricinfo scraper/scorecard.js
+++ ESPN Cricinfo scraper/scorecard.js
@@ -1,5 +1,3 @@
-
-
 //team name, venue,date,result
 const url = 'https://www.espncricinfo.com//series/ipl-2020-21-1210595/mumbai-indians-vs-chennai-super-kings-1st-match-1216492/full-scorecard'
 
@@ -33,7 +31,37 @@ function extractData(html) {
     //trim method is used to remove white/wide spaces
     let result = $('.match-info.match-info-MATCH.match-info-MATCH-half-width .status-text span').text()
 
-    console.log(venue)
-    console.log(date)
-    console.log(result)
+    console.log(venue);
+    console.log(date);
+    console.log(result);
+
+    console.log('....................................');
+
+    let innings = $('.card.content-block.match-scorecard-table>.Collapsible')
+    //console.log(innings);
+    let htmlString = ''
+
+    for (let i = 0; i < innings.length; i++) {
+        htmlString += $(innings[i]).html()
+
+        let teamName = $(innings[i]).find('h5').text()
+
+
+        teamName = teamName.split('INNINGS')[0].trim()
+        //    teamName = teamName.split('INNINGS')
+        //    teamName = teamName[0]
+        let opponentIndex = i == 0 ? 1 : 0;
+        let opponentName = $(innings[opponentIndex]).find('h5').text()
+        opponentName = opponentName.split('INNINGS')[0].trim()
+
+        //console.log(teamName, "->", opponentName)
+
+        let currInning = $(innings[i]);
+        
+    }
+    //console.log(htmlString)
+
 }
+
+
+
