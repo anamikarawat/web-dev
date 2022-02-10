58c9592592656812b48c5cfd1ab1a2d660c06383
anamikarawat <anamikarawart@gmail.com>
2022-02-10 21:58:37 +0530

    add

---
+++ ESPN Cricinfo scraper/funWithJson.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+let buffer = fs.readFileSync('./example.json')
+
+//console.log(buffer)
+
+let data = JSON.parse(buffer)
+console.log(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
