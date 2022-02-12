99240925886168aecb0799b829362dae9a0b5cdf
anamikarawat <anamikarawart@gmail.com>
2022-02-12 19:40:30 +0530

    add

--- ESPN Cricinfo scraper/main.js
+++ ESPN Cricinfo scraper/main.js
@@ -10,11 +10,11 @@ const path = require('path')
 const allMatchObj = require("./allMatch");
 
 
-let iplPath = path.join(__dirname , "IPL")
+let iplPath = path.join(__dirname, "IPL")
 //__dirname-> jo hamari current directory hai uska path leke ajata h == parent direcorty Path-> ESPN cricinfo scraper
 
-function dirCreator(filePath){
-  if(fs.existsSync(filePath)==false){
+function dirCreator(filePath) {
+  if (fs.existsSync(filePath) == false) {
     fs.mkdirSync(filePath)
   }
 }
@@ -26,27 +26,27 @@ dirCreator(iplPath)
 request(url, cb)
 
 function cb(error, response, html) {
-    if (error) {
-        console.error(error)
-    }
-    else {
-        extractLink(html);
-        // console.log(html)
-    }
+  if (error) {
+    console.error(error)
+  }
+  else {
+    extractLink(html);
+    // console.log(html)
+  }
 }
 //view all results link- ek anchor tag h which have href hoga usmea site ka link
 function extractLink(html) {
-    let $ = cheerio.load(html)
-    //half link
-    let anchorElem = $('a[data-hover="View All Results"]')
-    let link = anchorElem.attr('href')
-    //attr - attributes
-    //console.log(link)
+  let $ = cheerio.load(html)
+  //half link
+  let anchorElem = $('a[data-hover="View All Results"]')
+  let link = anchorElem.attr('href')
+  //attr - attributes
+  console.log(link)
 
-    //full link- concatination
-    let fullLink = "https://www.espncricinfo.com/" + link;
-    console.log(fullLink);
+  //full link- concatination
+  let fullLink = "https://www.espncricinfo.com/" + link;
+  console.log(fullLink);
 
-    allMatchObj.getAllMatch(fullLink);
+  allMatchObj.getAllMatch(fullLink);
 }
 //score table for one particular match - in scorecard.js
