b0087d8007336eb50a02102b93f8ca37c948e1b4
anamikarawat <anamikarawart@gmail.com>
2022-02-19 15:35:05 +0530

    add

--- JS CONCEPTS/Higher order function/Reduce.js
+++ JS CONCEPTS/Higher order function/Reduce.js
@@ -3,22 +3,22 @@
 
 const arr = [2, 3, 4, 5, 6]
 
-// let addition = arr.reduce(function(sum, value){
-
-//     let updatedSum = sum+value
-//     return updatedSum
-// },0)
-
-// console.log(addition)
-
 let addition = arr.reduce(function(sum, value){
 
-    let Sum = sum+value
-    return Sum
+    let updatedSum = sum+value
+    return updatedSum
+},0)
+//=============================================================
+console.log(addition)
+
+let addition = arr.reduce(function(sum, value){  //sum -> accumulator,initially its value is 0 then its value updated after that
+
+     sum = sum+value
+    return sum
 },0)
 
 console.log(addition)
-//===========================
+//=========================================================
 let product = arr.reduce(function(multiply, value){
 
     let updatedProduct = multiply*value
@@ -26,6 +26,4 @@ let product = arr.reduce(function(multiply, value){
 },1)
 
 console.log(product)
-
-
-//t.me/+EbBrL8gl-RZmNjZl                  telegram group
+                
--- JS CONCEPTS/ReduceRight.js
+++ JS CONCEPTS/ReduceRight.js
@@ -1,13 +1,18 @@
 //This will work exactly as Reduce the only thing is that it iterate over the array from right to left
 
 let arr = [2 , 3, 4, 5, 6]
-
-
 let sumFromRight = arr.reduceRight(function(acc , value){
-      let upadtedSum = acc+value
-      console.log(upadtedSum)
-      return upadtedSum
+      return acc+value
 } , 0)
 
 
 console.log(sumFromRight)
+//================================================
+// let sumFromRight = arr.reduceRight(function(acc , value){
+//       let upadtedSum = acc+value
+//       console.log(upadtedSum)
+//       return upadtedSum
+// } , 0)
+
+
+// console.log(sumFromRight)
