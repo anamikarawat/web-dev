df7009b33c701e3b8ccdec21585b56277e9af7bd
anamikarawat <anamikarawart@gmail.com>
2022-02-21 01:38:20 +0530

    add

--- Async JS/Promises/readingFilesSerially.js
+++ Async JS/Promises/readingFilesSerially.js
@@ -1,23 +1,54 @@
 let fs = require("fs");
 
+// console.log("before");
+
+// let f1p = fs.promises.readFile("f1.txt");
+
+// f1p.then(cb);
+
+// function cb(data) {
+//   console.log("File data -> " + data);
+//   let f2p = fs.promises.readFile("f2.txt");
+
+//   f2p.then(cb2);
+// }
+
+// function cb2(data) {
+//   console.log("File data -> " + data);
+//   let f3p = fs.promises.readFile("f3.txt");
+
+//   f3p.then(cb3);
+// }
+
+// function cb3(data) {
+//   console.log("File data -> " + data);
+// }
+
+// console.log("after");
+
+//=========================================================================
+//improvement
+
 console.log("before");
 
 let f1p = fs.promises.readFile("f1.txt");
 
-f1p.then(cb);
+f1p.then(cb).then(cb2).then(cb3).catch(function(error){
+  console.log(err)
+})
 
 function cb(data) {
   console.log("File data -> " + data);
   let f2p = fs.promises.readFile("f2.txt");
 
-  f2p.then(cb2);
+  return f2p
 }
 
 function cb2(data) {
   console.log("File data -> " + data);
   let f3p = fs.promises.readFile("f3.txt");
 
-  f3p.then(cb3);
+  return f3p
 }
 
 function cb3(data) {
